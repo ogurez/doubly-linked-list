6f46fb2c6d3103c72a2a4b46ea7c906e884db3ff
ogurez <ogurezslava@gmail.com>
2017-01-26 19:30:42 +0300

    Initial commit

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,99 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+      this._head=new Node("HEAD", null, null);
+      this._tail=new Node("TAIL", null, null);
 
-    append(data) {}
+      this._head.next=this._tail;
+      this._tail.prev=this._head;
 
-    head() {}
+      this.length=0;
+    }
 
-    tail() {}
+    append(data) {
+      this.insertAt(this.length,data);
+      return this;
+    }
 
-    at(index) {}
+    head() {
+      return this._head.next.data;
+    }
 
-    insertAt(index, data) {}
+    tail() {
+      return this._tail.prev.data;
+    }
+
+    at(index) {
+      if (isNaN(index)|| index> this.length){
+        throw new Error('Invalid index.');
+      }
+
+      var current = this._head.next,
+      counter = 0;
+
+      while (index !=counter && counter<=this.length){
+        current=current.next;
+        counter++
+      }
+      return current;
+    }
+
+    insertAt(index, data) {
+      var position= this._at(index),
+      newNode=new Node(data, position.prev, position);
+
+      position.prev=newNode;
+      newNode.prev.next=newNode;
+      this.length++;
+
+      return this;
+    }
 
     isEmpty() {}
 
     clear() {}
 
-    deleteAt(index) {}
+    deleteAt(index) {
+      var position = this._at(index);
 
-    reverse() {}
+      postion.next.prev=position.prev;
+      position.prev.next=position.next;
+      this.length--;
 
-    indexOf(data) {}
+      position.prev=null;
+      position.next=null;
+      position.data=null;
+
+      return this;
+    }
+
+    reverse() {
+      var currenth=this._head.next,
+      currentt=this._tail.prev,
+      index=0, tmp;
+
+      while(index!=Math.floor(this.index/2)){
+        tmp=currenth.data;
+        currenth.data=currentt.data;
+        currentt.data=tmp;
+
+        currenth=currenth.next;
+        currentt=currentt.prev;
+      }
+      return this;
+    }
+
+    indexOf(data) {
+      var index=0,
+      current=this._head.next;
+
+      while(current!== null && current.data!==data){
+        current=current.next;
+        index++;
+      }
+      return current === null ? -1 : index;
+    }
 }
 
 module.exports = LinkedList;
